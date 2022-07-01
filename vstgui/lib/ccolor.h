beda8a0ac396ea36f2652db27101c68e463a16fb
rjonkman <yeahtuna@gmail.com>
2022-07-01 11:58:37 +0800

    Test Commiting from Linux

--- vstgui/lib/ccolor.h
+++ vstgui/lib/ccolor.h
@@ -24,7 +24,7 @@ struct CColor
 	: red (inColor.red), green (inColor.green), blue (inColor.blue), alpha (inColor.alpha)
 	{}
 
-	//TEST PUSH
+    //TEST PUSH 2
 	constexpr CColor (const uint32_t color)
     #if WINDOWS || defined(__LITTLE_ENDIAN__) || defined(__LITTLE_ENDIAN)
 	: red ((color >> 24) & 0xFF), green ((color >> 16) & 0xFF), blue (blue = (color >> 8) & 0xFF), alpha (color & 0xFF)
